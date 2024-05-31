948d5dae7dade7fb5374d8ccabab422a1776ace6
hardi-yansah <hardi.yansah97@gmail.com>
2024-05-31 14:14:52 +0700

    feat: Add TC & TS

--- Test Suites/TS_001_Register.ts
+++ Test Suites/TS_001_Register.ts
@@ -12,31 +12,80 @@
    <rerunImmediately>false</rerunImmediately>
    <testSuiteGuid>511c1cd4-cf30-4a1d-b9f8-6a4ceeba65c3</testSuiteGuid>
    <testCaseLink>
-      <guid>07004bdb-f105-426d-974f-c5161824dec7</guid>
+      <guid>8c0b3f07-444c-4c9e-8910-f54b0a004f3a</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/Register/TC_001_Register New User</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
-      <guid>e1a62dfb-018d-46d2-99aa-c88002a31b3e</guid>
+      <guid>86f0fed2-16a4-4a0c-9329-63553f95b08e</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/Register/TC_002_Register No First Name</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
-      <guid>9678e0d9-fffa-473c-bd2f-b5270ba21b7f</guid>
+      <guid>bb767348-d7c9-47ca-a3a6-9c3b83d3f3b6</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/Register/TC_003_Register No Last Name</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
-      <guid>2c462d70-459e-44d8-8f15-8e22ab9148e8</guid>
+      <guid>c6bfcc63-bb89-4daf-8af3-cfeb4b9aa79f</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/Register/TC_004_Register No Address Street</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>023b636f-e993-493b-929e-4c0fa85d416a</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Register/TC_005_Register No Address City</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>c5484313-b567-447d-820a-ec9e6557a1a2</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Register/TC_006_Register No Address State</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>6433ac04-afff-48d6-8484-0355316637cc</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Register/TC_007_Register No Address Zip Code</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>b6ecfc77-bfc4-4217-a3c6-f5f64d3d1912</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Register/TC_008_Register No SSN</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>543d4183-2b07-4967-b121-ea0931ef0191</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Register/TC_009_Register No Username</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>f9a6810b-5fe0-4965-ad20-9f9cf719b2d3</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Register/TC_010_Register No Password</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>81917293-df63-4901-82b1-a4a812101207</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Register/TC_011_Register No Confirm Password</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
 </TestSuiteEntity>
